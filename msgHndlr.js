4fe0d18845c99d9a3ccf57454a67cda9f7f7b0e4
Tobz <tobyprogamerz@gmail.com>
2020-10-05 08:10:16 -0700

    Add files via upload

--- msgHndlr.js
+++ msgHndlr.js
@@ -84,7 +84,7 @@ module.exports = msgHandler = async (client, message) => {
         const groupAdmins = isGroupMsg ? await client.getGroupAdmins(groupId) : ''
         const isGroupAdmins = isGroupMsg ? groupAdmins.includes(sender.id) : false
         const isBotGroupAdmins = isGroupMsg ? groupAdmins.includes(botNumber + '@c.us') : false
-        const ownerNumber = '6281311850715x@c.us'
+        const ownerNumber = '6281311850715@c.us'
         const isOwner = sender.id === ownerNumber
         const isBlocked = blockNumber.includes(sender.id)
         const isNsfw = isGroupMsg ? nsfw_.includes(chat.id) : false
