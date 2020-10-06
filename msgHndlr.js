e1da88f73e1c89eb9ad6ef3b96e7b9c896c2a75d
Tobz <tobyprogamerz@gmail.com>
2020-10-06 06:54:22 -0700

    Add files via upload

--- msgHndlr.js
+++ msgHndlr.js
@@ -681,12 +681,12 @@ module.exports = msgHandler = async (client, message) => {
             const isGroupOwner = sender.id === chat.groupMetadata.owner
             if (!isGroupOwner) return client.reply(from, 'Perintah ini hanya bisa di gunakan oleh Owner group', id)
             if (!isBotGroupAdmins) return client.reply(from, 'Perintah ini hanya bisa di gunakan ketika bot menjadi admin', id)
-            const allMem = await client.getGroupMembers(groupId)
-            for (let i = 0; i < allMem.length; i++) {
-                if (groupAdmins.includes(allMem[i].id)) {
+            const allMek = await client.getGroupMembers(groupId)
+            for (let i = 0; i < allMek.length; i++) {
+                if (groupAdmins.includes(allMek[i].id)) {
                     console.log('Upss this is Admin group')
                 } else {
-                    await client.removeParticipant(groupId, allMem[i].id)
+                    await client.removeParticipant(groupId, allMek[i].id)
                 }
             }
             client.reply(from, 'Succes kick all member', id)
